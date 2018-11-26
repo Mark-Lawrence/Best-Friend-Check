6649429ecc513c59b2767349c566dbc207aacb88
Mark Lawrence <mark488@ufl.edu>
2018-11-26 15:45:36 -0500

    Initial Commit

---
+++ Best Friend Check/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Best Friend Check
+//
+//  Created by Mark Lawrence on 11/26/18.
+//  Copyright © 2018 Mark Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
